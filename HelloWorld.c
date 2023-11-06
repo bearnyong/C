d9e6e7a0c28298c7bc7300a807bd64250588cf97
bearnyong <rhalsdud216@gmail.com>
2023-11-06 21:46:09 +0900

    hello world

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Ctrl+Alt+C -> 컴파일러 선택\n");
+    printf("Ctrl+Alt+R -> 실행\n");
+
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+
+    return 0;
+}
